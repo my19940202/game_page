d808014245321c21908ec8fcdd0980a7aaa79ac6
xibobo <xishengbo@baidu.com>
2019-08-31 11:05:14 +0800

    抽奖前输入姓名 减少异常抽奖情况

--- asset/rect.js
+++ asset/rect.js
@@ -31,9 +31,16 @@ function highlighter(idx) {
 var idx = 0;
 $('.btn').click(function () {
     if (localStorage.getItem('prized') == 1) {
-        alert(' 已经抽过奖了');
+        alert('已经抽过奖了');
+        return;
     }
-    if (localStorage.getItem('prized') == 0 && !running) {
+    let username = checkUserName();
+    if (username === '') {
+        alert('请输入邮箱前缀');
+        return;
+    }
+
+    if (localStorage.getItem('prized') === '0' && !running) {
         idx = 0;
         running = true;
         var maxNum = 8 + Math.floor(Math.random() * 16);
@@ -41,7 +48,7 @@ $('.btn').click(function () {
             if (idx > maxNum) {
                 timer = clearInterval(timer);
                 running = false;
-                getprize();
+                getprize(username);
             }
             highlighter(idx % 8);
             idx++;
@@ -49,23 +56,23 @@ $('.btn').click(function () {
     }
 });
 
-function getprize() {
-
+function getprize(username) {
     if (localStorage.getItem('prized') == 1) {
         return;
     }
 
-    $.get(api + 'path=getprize', function(data, status){
-        if (status == 'success') {
+    $.get(api + 'path=getprize', (data, status) => {
+        if (status === 'success') {
             if (data.result) {
-                if (data.result == '游戏已经结束') {
+                if (data.result === '游戏已经结束') {
                     alert('抽奖活动已经活动已结束~~');
                     return;
                 } else {
-                    dispModal(data.result, data.idx);
+                    // 发送中奖请求
+                    lockPrice(username, data.result, data.idx);
                 }
             } else {
-                alert('服务异常,请重试');    
+                alert('服务异常,请重试');
             }
         } else {
             alert('服务异常');
@@ -75,7 +82,7 @@ function getprize() {
 
 // 抽奖提示
 let mask = document.querySelector('.mask');
-function dispModal(result, idx) {
+function dispModal(result) {
     let imgMap = {
         '小夜灯': {
             img: 'light',
@@ -99,29 +106,36 @@ function dispModal(result, idx) {
     document.getElementById('title').innerHTML = imgMap[result].desc;
     // set img
     document.getElementById('img').src = './gift/' + imgMap[result].img + '.jpg';
-    window.result = result;
-    window.idx = idx;
 }
-let submitBtn = document.getElementById('submitBtn');
-submitBtn.addEventListener('click', () => {
-    if (localStorage.getItem('prized') == 1) {
-        alert(' 已经抽过奖了');
-    }
-    let name = document.getElementById('submit').value;
-    if (name && name != '') {
-        var reqStr = 'path=submit&name=' + name + '&result=' + window.result + '&idx=' + window.idx;
-        $.get(api + reqStr, function (data, status) {
-            if (status == 'success') {
-                localStorage.setItem('prized', 1);
-                alert('提交成功');
-                setTimeout(() => {
-                    mask.style.display = 'none';
-                }, 500);
-            } else {
-                alert('提交异常');
-            }
-        });
-    } else {
-        alert('名字异常');
-    }
+let confirmBtn = document.getElementById('confirmBtn');
+confirmBtn.addEventListener('click', () => {
+    mask.style.display = 'none';
 });
+
+// 用户输入校验
+function checkUserName(params) {
+    let input = $('#username').val().trim();
+    let ret = '';
+    // TODO: 是否满足邮件组配置
+    var mailGroup = [];
+    if (input) {
+        ret = input;
+    }
+    return ret;
+}
+
+function lockPrice(name, result, idx) {
+    let reqStr = 'path=submit&name=' + name
+        + '&result=' + result
+        + '&idx=' + idx;
+    $.get(api + reqStr, function (data, status) {
+        if (status === 'success') {
+            localStorage.setItem('prized', 1);
+            setTimeout(() => {
+                dispModal(result, idx);
+            }, 500);
+        } else {
+            alert('抽奖异常,请重试');
+        }
+    });
+}
